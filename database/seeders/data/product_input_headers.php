51c4d716c91e66536bf5797dc374392f1b289213
salah <cloud.data.sa@gmail.com>
2025-06-10 12:55:58 +0300

    dsa

--- database/seeders/data/product_input_headers.php
+++ database/seeders/data/product_input_headers.php
@@ -11,7 +11,7 @@ return [
 ['id'=>7,'total_price'=>359530,'bill_number'=>2476,'vendor_id'=>null,'created_at'=>'2025-06-07 08:21:15','updated_at'=>'2025-06-10 08:57:11',],
 ['id'=>8,'total_price'=>16600,'bill_number'=>2461,'vendor_id'=>null,'created_at'=>'2025-06-08 00:58:07','updated_at'=>'2025-06-10 08:57:11',],
 ['id'=>9,'total_price'=>48580,'bill_number'=>55037,'vendor_id'=>null,'created_at'=>'2025-06-09 04:50:28','updated_at'=>'2025-06-10 08:57:11',],
-['id'=>10,'total_price'=>33800,'bill_number'=>231322,'vendor_id'=>null,'created_at'=>'2025-06-10 05:22:28','updated_at'=>'2025-06-10 08:57:11',],
+['id'=>10,'total_price'=>77000,'bill_number'=>231322,'vendor_id'=>null,'created_at'=>'2025-06-10 05:22:28','updated_at'=>'2025-06-10 09:55:44',],
 ['id'=>11,'total_price'=>600000,'bill_number'=>137,'vendor_id'=>null,'created_at'=>'2025-06-10 05:29:34','updated_at'=>'2025-06-10 05:31:33',],
 ['id'=>12,'total_price'=>13300,'bill_number'=>672020,'vendor_id'=>null,'created_at'=>'2025-06-10 05:31:59','updated_at'=>'2025-06-10 08:57:11',],
 ['id'=>13,'total_price'=>63100,'bill_number'=>123,'vendor_id'=>null,'created_at'=>'2025-06-10 09:26:42','updated_at'=>'2025-06-10 09:52:23',],
